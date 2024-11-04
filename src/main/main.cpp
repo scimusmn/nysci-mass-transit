61ae77ddf751a8c9a543817d8cf40596e25dc64f
sanine <sanine.not@pm.me>
2024-11-04 08:17:40 -0600

    add Grid functions

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <random>
 #include <opencv2/core.hpp>
 #include <opencv2/videoio.hpp>
@@ -15,8 +16,109 @@
 struct cag_option options[] = {
   { 'f', "f", NULL, "CONFIG_FILE", "config file to read (default " DEFAULT_CONFIG_FILE ")" },
   { 'v', "v", "visualize", NULL, "visually display the processing" },
+  { 'c', "c", "colors", NULL, "visually display the color processing" },
   { 'h', "h", "help", NULL, "display help message" },
 };
+
+
+struct Visualization {
+  bool main;
+  bool colors;
+};
+
+
+#define GRID_W 10
+#define GRID_H 10
+#define BUMP_RATE 3
+#define DRAIN_RATE 1
+
+
+struct PointScore {
+  double x, y;
+  unsigned int score = 0;
+};
+struct Grid {
+  std::vector<PointScore> x[GRID_W][GRID_H];
+  std::vector<PointScore> * operator[](int idx) {
+    return x[idx];
+  }
+};
+
+
+cv::Point gridSquare(PointScore p) {
+  cv::Point pt;
+  pt.x = floor(GRID_W * p.x);
+  pt.y = floor(GRID_H * p.y);
+  return pt;
+}
+
+
+void insertPoint(Grid &grid, PointScore p) {
+  cv::Point square = gridSquare(p);
+  grid[square.x][square.y].push_back(p);
+}
+
+std::vector<PointScore> elementsAt(Grid &grid, cv::Point square) {
+  if (
+    (square.x >= 0 && square.x < GRID_W) &&
+    (square.y >= 0 && square.y < GRID_H)
+  ) {
+    return grid[square.x][square.y];
+  } else {
+    return std::vector<PointScore>(0);
+  }
+}
+
+
+double dist2(PointScore a, PointScore b) {
+  double dx = a.x - b.x;
+  double dy = a.y - b.y;
+  return (dx*dx) + (dy*dy);
+}
+
+
+void updateCircle(Grid &grid, PointScore p, double r) {
+  bool match = false;
+  double r2 = r*r;
+  cv::Point square = gridSquare(p);
+  for (int dx = -1; dx <= 1; dx++) {
+    for (int dy = -1; dy <= 1; dy++) {
+      cv::Point s = { square.x + dx, square.y + dy };
+      auto e = elementsAt(grid, s);
+      for (auto it = e.begin(); it != e.end(); it++) {
+        if (dist2(*it, p) < r2) {
+          it->score += BUMP_RATE;
+          match = true;
+        }
+      }
+    }
+  }
+
+  if (!match) {
+    p.score = BUMP_RATE;
+    insertPoint(grid, p);
+  }
+}
+
+
+void updateGrid(Grid &grid) {
+  for (int x = 0; x<GRID_W; x++) {
+    for (int y = 0; y<GRID_H; y++) {
+      auto &e = grid[x][y];
+      for (auto it = e.begin(); it != e.end();) {
+        it->score -= DRAIN_RATE;
+        if (it->score <= 0) {
+          it = e.erase(it);
+        } else {
+          it++;
+        }
+      }
+    }
+  }
+}
+
+
+
 
 
 #define BELOW(x, hi) x = x < hi ? x : hi-1;
@@ -43,7 +145,6 @@ void buildColorMasks(cv::Mat frame, MaskVector &masks, Colors colors) {
   for (auto it = colors.data.begin(); it != colors.data.end(); it++) {
     cv::Mat m;
     hsvMask(frame, m, it->second);
-    std::cout << "mat type: " << m.type() << std::endl;
     masks.push_back(std::pair<std::string, cv::Mat>(it->first, m));
   }
 }
@@ -63,10 +164,8 @@ cv::Point randomCirclePoint(cv::Vec3f v, Random &rand) {
 void updateColorHistogram(std::vector<int> &histogram, cv::Point p, MaskVector &masks) {
   for (auto it = masks.begin(); it != masks.end(); it++) {
     if (it->second.at<uchar>(p) != 0) {
-      std::cout << "color match: " << it->first << std::endl;
       auto idx = it - masks.begin();
       histogram[idx] += 1;
-      std::cout << idx << ": " << histogram[idx] << std::endl;
     }
   }
 }
@@ -99,7 +198,8 @@ std::string getCircleColor(cv::Vec3f circle, MaskVector &masks, int iterations, 
 }
 
 
-void loop(cv::VideoCapture &camera, Configuration &config, Random &rand, bool visualize) {
+void loop(cv::VideoCapture &camera, Configuration &config, Random &rand, Visualization viz) {
+  auto startTime = std::chrono::high_resolution_clock::now();
   cv::Mat frame, gray;
   camera >> frame;
   cv::warpPerspective(frame, frame, config.projection, frame.size());
@@ -107,7 +207,7 @@ void loop(cv::VideoCapture &camera, Configuration &config, Random &rand, bool vi
 
   MaskVector masks;
   buildColorMasks(frame, masks, config.colors);
-  if (visualize) {
+  if (viz.colors) {
     for (auto it = masks.begin(); it != masks.end(); it++) {
       cv::imshow(it->first, it->second);
     }
@@ -118,7 +218,7 @@ void loop(cv::VideoCapture &camera, Configuration &config, Random &rand, bool vi
   std::vector<std::string> circleColors;
   for (auto it = circles.begin(); it != circles.end(); it++) {
     circleColors.push_back(getCircleColor(*it, masks, 50, rand));
-    if (visualize) {
+    if (viz.main) {
       cv::circle(frame, cv::Point((*it)[0], (*it)[1]), (*it)[2], CV_RGB(0, 255, 0));
       cv::putText(
         frame, circleColors[circleColors.size()-1], cv::Point((*it)[0], (*it)[1]),
@@ -127,10 +227,13 @@ void loop(cv::VideoCapture &camera, Configuration &config, Random &rand, bool vi
     }
   }
 
-  if (visualize) {
+  if (viz.main) {
     cv::imshow("camera", frame);
     cv::waitKey(1);
   }
+  auto endTime = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+  std::cout << "processed frame in " << duration.count() << " ms" << std::endl;
 }
 
 
@@ -139,7 +242,7 @@ int main(int argc, char **argv) {
   Random rand;
   Configuration config;
   const char *filename = DEFAULT_CONFIG_FILE;
-  bool visualize = false;
+  Visualization viz = { false, false };
   
   // parse command-line options
   cag_option_context ctx;
@@ -150,7 +253,10 @@ int main(int argc, char **argv) {
         filename = cag_option_get_value(&ctx);
         break;
       case 'v':
-        visualize = true;
+        viz.main = true;
+        break;
+      case 'c':
+        viz.colors = true;
         break;
       case 'h':
         printf("Usage: mt-input [OPTION]...\n");
@@ -173,7 +279,7 @@ int main(int argc, char **argv) {
   openCamera(camera, config);
     
   while(true) {
-    loop(camera, config, rand, visualize);
+    loop(camera, config, rand, viz);
   }
 
   camera.release();
